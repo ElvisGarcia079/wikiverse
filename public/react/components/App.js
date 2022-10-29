abbef9a01977262ba9f0c5e3e1c12a4ed39cdcfb
Elvis <elvisgarcia93@gmail.com>
2022-10-29 17:27:31 -0400

    Add selectPage and setSelectPage state with Initial value of Null

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -8,6 +8,11 @@ export const App = () => {
 
 	// This state is used to store the value of the current page. Initially this value starts as an empty array. The useEffect hook instantly loads values into this array upon initial loading.
 	const [pages, setPages] = useState([]);
+
+	// 1. We are going to have to create a new state for keeping track of the article that is fetched from the database based on it's "slug" value. 
+	
+	// 2. The initial value of this state will be "null". That's because what we get back from the Database is an Object, a JS Object, and "null" is an object with no reference. Most importantly, the reason we will not set the initial state of this to an empty array like we did with the state above is, this will have an initial implicit value of "false", which will help with our conditional rendering. 
+	const [selectedPage, setSelectedPage] = useState(null);
 	
 	
 
